b7573bda483523a04ddd535e4f1ab726c0feec4c
dfmodev <dfmodev@protonmail.com>
2022-07-21 15:46:42 -0400

    test

---
+++ game.py
@@ -0,0 +1,16 @@
+import sys, pygame
+
+pygame.init()
+
+
+size = width, height = 320, 240
+
+
+
+screen = pygame.display.set_mode(size)
+
+while 1:
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT: sys.exit()
