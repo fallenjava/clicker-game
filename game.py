9a07afc967fd5d4fc70f8d68083c9b781c74c34b
mazen <mazenwontgethis@proton.me>
2022-07-21 16:07:42 -0400

    clicking system

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-import sys, pygame
+import pygame
 
 pygame.init()
 
@@ -6,10 +6,17 @@ size = width, height = 800, 600
 
 screen = pygame.display.set_mode(size)
 
+clicks = 0
 
 running = True
 while running:
 
     for event in pygame.event.get():
 
-        if event.type == pygame.QUIT: running = False
+        if event.type == pygame.QUIT: 
+            running = False
+
+        if event.type == KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                clicks += 1
+
