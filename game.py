cd562567e44bcde615d97a04fcf2b678632b3e93
dfmodev <dfmodev@protonmail.com>
2022-07-28 13:45:35 -0400

    Added achievements for 100 and 1000 Clicks!

--- game.py
+++ game.py
@@ -6,6 +6,7 @@ pygame.font.init()
 pygame.display.init()
 size = width, height = 800, 600
 font = pygame.font.Font('assets/Poppins-ExtraLight.ttf', 64)
+font2 = pygame.font.Font('assets/Poppins-ExtraLight.ttf', 48) 
 cursor = pygame.image.load("assets/cursor.png") 
 cursor_clicked = pygame.image.load("assets/cursor_click.png")
 icon = pygame.image.load('assets/icon.png')
@@ -13,6 +14,8 @@ screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Clicker Game")
 pygame.display.set_icon(icon)
 clicks = 0
+hundredclicks = ("Reached 100 Clicks")
+thousandclicks = ("Reached 1000 Clicks")
 
 running = True
 while running:
@@ -26,14 +29,19 @@ while running:
             if event.button == 1:
                 clicks += 1 
                 print(clicks)
-
-        if event.type == KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                clicks += 1
-                print(clicks)
+                if clicks == 100:
+                    print(hundredclicks)
+                if clicks == 1000:
+                    print(thousandclicks)
+        
+   
         
             
     text = font.render("Clicks: " + str(clicks), True, (0, 0, 0))
+    if clicks == 100:
+        text = font2.render(str(hundredclicks), True, (0, 0, 0))
+    elif clicks == 1000:   
+        text = font2.render(str(thousandclicks), True, (0, 0, 0))
     screen.fill((255, 255, 255))
     screen.blit(text, (269, 100))
     pygame.display.flip()
